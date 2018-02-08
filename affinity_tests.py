fbb134002acc10d9d9fa3fea8e7df4fcb88d099d
Rogozin Dmitrii <rogozin@amazon.com>
2018-02-08 12:20:31 +0100

    Add product bullets and image block tests

--- affinity_tests.py
+++ affinity_tests.py
@@ -17,14 +17,38 @@ class QpeTest(base_test.BaseTest):
         super().__init__(*args, **kwargs)
         self.url = "https://tr-pre-prod.amazon.com/dp/B01BTZFM0W"
 
-    def runTest(self):
+    #Test content of the QPE Title Tag
+    def test_qpe_title_tag(self):
         body = self.driver.find_elements_by_tag_name("body")[0]
         qpeTitleElem = body.find_element_by_id("qpeTitleTag_feature_div")
         actual = qpeTitleElem.text
-        expectedContent = "foo"
+        expectedContent = "Amazon'a özel cep telefonu fırsatlarını buradan keşfedin."
         asin = "B01BTZFM0W"
         self.assertEqual(actual,expectedContent, f"ASIN {asin} has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ")
 
+    #Test features bullets has 5 or more items
+    def test_feature_bullets_is_not_empty(self):
+        body = self.driver.find_elements_by_tag_name("body")[0]
+        elem = body.find_element_by_id("featurebullets_feature_div")
+        contentList = elem.find_elements_by_css_selector("li")
+        expectedNElements= 5;
+        self.assertGreaterEqual(len(contentList),expectedNElements, f"The feature bullets should have at least {expectedNElements} items")
+
+
+    # test image block has non empty images
+    def test_image_block_alt_images_are_not_empty(self):
+        body = self.driver.find_elements_by_tag_name("body")[0]
+        elem = body.find_element_by_id("imageBlock_feature_div")
+        contentList = elem.find_elements_by_id("altImages")
+
+        self.assertGreater(len(contentList),0, "altImages size should be more  than 0")
+        images = contentList[0].find_elements_by_css_selector("img");
+        self.assertGreater(len(images), 0, "There should be more than 0 images in altImages.")
+        expectedWidth = 10
+        #for some reason first element has 0x0 size, excluding it.
+        for image in images[1:len(images)]:
+            width = image.size['width'];
+            self.assertGreaterEqual(width, expectedWidth, f"Image size should be bigger than {expectedWidth}")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(QpeTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
