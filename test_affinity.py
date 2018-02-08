4fad0fd0baa3b5371c6e03db9cac60abafd47e3a
Igor Novak <novakin@amazon.com>
2018-02-08 14:59:07 +0100

    Support python 3.4: replace f-strings with format method

--- test_affinity.py
+++ test_affinity.py
@@ -16,14 +16,14 @@ class QpeTest(base_test.BaseTest):
         qpeTitleElem = self.body.find_element_by_id("qpeTitleTag_feature_div")
         actual = qpeTitleElem.text
         expectedContent = "Amazon'a özel cep telefonu fırsatlarını buradan keşfedin."
-        self.assertEqual(actual,expectedContent, f"ASIN has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ")
+        self.assertEqual(actual,expectedContent, "ASIN has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ".format(expectedContent=expectedContent, actual=actual))
 
     #Test features bullets has 5 or more items
     def test_feature_bullets_is_not_empty(self):
         elem = self.body.find_element_by_id("featurebullets_feature_div")
         contentList = elem.find_elements_by_css_selector("li")
         expectedNElements= 5;
-        self.assertGreaterEqual(len(contentList),expectedNElements, f"The feature bullets should have at least {expectedNElements} items")
+        self.assertGreaterEqual(len(contentList),expectedNElements, "The feature bullets should have at least {expectedNElements} items".format(asin=asin, expectedNElements=expectedNElements, actual=actual))
 
 
     # test image block has non empty images
@@ -38,7 +38,7 @@ class QpeTest(base_test.BaseTest):
         #for some reason first element has 0x0 size, excluding it.
         for image in images[1:len(images)]:
             width = image.size['width'];
-            self.assertGreaterEqual(width, expectedWidth, f"Image size should be bigger than {expectedWidth}")
+            self.assertGreaterEqual(width, expectedWidth, "Image size should be bigger than {expectedWidth}".format(asin=asin, expectedWidth=expectedWidth, actual=actual))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(QpeTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- test_title.py
+++ test_title.py
@@ -15,13 +15,13 @@ class TitleTest(base_test.BaseTest):
     #     actual = self.body.find_element_by_id("title_feature_div").text
     #     expectedContent = ""
     #     asin = "B01BTZFM0W"
-    #     self.assertEqual(actual,expectedContent, f"ASIN {asin} has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ")
+    #     self.assertEqual(actual,expectedContent, "ASIN {asin} has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ".format(asin=asin, expectedContent=expectedContent, actual=actual))
 
     def test_title(self):
         actual = self.body.find_element_by_id("title_feature_div").text
         expectedContent = "Samsung Galaxy S7 akıllı telefon, 5,1 inç (12,9 cm) dokunmatik ekran, 32 GB dahili hafıza, Android OS, siyah., 32 GB, Siyah"
         asin = "B01BTZFM0W"
-        self.assertEqual(actual, expectedContent, f"ASIN {asin} has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ")
+        self.assertEqual(actual, expectedContent, "ASIN {asin} has wrong qpeTitle Value. exp {expectedContent}, actual {actual} ".format(asin=asin, expectedContent=expectedContent, actual=actual))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TitleTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
