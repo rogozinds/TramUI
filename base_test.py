40a4604998906b8ca4562e6a551d27e913ba7828
Igor Novak <novakin@amazon.com>
2018-02-08 13:00:34 +0100

    Setup cookies in BaseTest constructor

--- base_test.py
+++ base_test.py
@@ -8,7 +8,8 @@ import unittest
 #testQPETag(ASIN_TO_CONTENT)
 class BaseTest(unittest.TestCase):
 
-    def openPage(self, url):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         SERVICE_ARGS = ['--ignore-ssl-errors=true', '--ssl-protocol=any']
         dcap = dict(DesiredCapabilities.PHANTOMJS)
         # emulate userAgent as OPF will return empty webpage for "bots"
@@ -16,16 +17,15 @@ class BaseTest(unittest.TestCase):
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
             "(KHTML, like Gecko) Chrome/15.0.87"
         )
-        driver = webdriver.PhantomJS(service_args=SERVICE_ARGS, desired_capabilities=dcap)
-        driver.set_window_size(1024, 768)  # optional
-        driver.get(url)
-        if len(driver.find_elements_by_tag_name("body")[0].find_elements_by_tag_name("div")) == 0:
-            raise AssertionError("The body of the page has no divs ")
-        return driver
+        self.driver = webdriver.PhantomJS(service_args=SERVICE_ARGS, desired_capabilities=dcap)
+        self.driver.set_window_size(1024, 768)  # optional
+        self.url = "https://tr-pre-prod.amazon.com/dp/B01BTZFM0W"
 
     #Method of the parent class, that will be called before run test
     def setUp(self):
-        self.driver = self.openPage(self.url)
+        self.driver.get(self.url)
+        if len(self.driver.find_elements_by_tag_name("body")[0].find_elements_by_tag_name("div")) == 0:
+            raise AssertionError("The body of the page has no divs ")
 
     def tearDown(self):
         # self.driver.save_screenshot("screen.png")
